2479b79b4ac1e6f747663fd0bb5f8b437947929c
Tiago Rocha <tiagofarocha@yahoo.com.br>
2018-11-10 17:53:33 -0200

    build and readme

---
+++ build/precache-manifest.269a5e3eeb915690962114353cafdca5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea365d276cc18db4ce7a",
+    "url": "/static/js/main.ea365d27.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "ea365d276cc18db4ce7a",
+    "url": "/static/css/main.79834e21.chunk.css"
+  },
+  {
+    "revision": "0893dd623809d31118d4f787191008d1",
+    "url": "/index.html"
+  }
+];
